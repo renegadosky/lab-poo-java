623db2c0ae19e774bdf8e1c303da082574d67ea5
renegadosky <raulvictorcdd@gmail.com>
2024-07-19 14:58:14 +0000

    exercicios

--- lab02/Entrada1.java
+++ lab02/Entrada1.java
@@ -1,10 +1,9 @@
 package lab02;
 public class Entrada1 {
-    // Não execute no 'Run'. Execute no Terminal
-    // java 02-argumentos/Argumentos1.java romulo
+    
     public static void main(String[] args) {
-        var aresta = Integer.parseInt(args[0]);
-        var volume = Math.pow(aresta, 3);
-        System.out.println("O volume do cubo é " + volume );
+        var raio = Integer.parseInt(args[0]);
+        var volume = 1.333 * Math.PI * Math.pow(raio, 2);
+        System.out.println("O volume do esfera é " + Math.round(volume));
     }
 }
--- lab04/Repeticoes1.java
+++ lab04/Repeticoes1.java
@@ -1,15 +1,17 @@
 package lab04;
+import java.util.Scanner;
 
 public class Repeticoes1 {
     // java lab04/Repeticoes1.java
     public static void main(String[] args) {
-        /**
-         * while: Executa um bloco de código enquanto uma condição específica for
-         * verdadeira.
-         */
-        var numero = 0;
+        var contador = 1;
+        var sc = new Scanner(System.in);
+        System.out.println("numero: ");
+        var numero = sc.nextInt();
         while (numero < 10) {
-            System.out.println(numero);
+            System.out.print(numero + "x" + contador);
+            System.out.print(" ");
+            System.out.print(numero * contador);
             numero += 1;
         }
     }
